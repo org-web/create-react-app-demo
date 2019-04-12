a62ac61e6ed6e0499a9c72172f7af6b1abdb7db5
liuyx <2479158033@qq.com>
2019-04-12 09:29:10 +0800

    feat: 集成eslint

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,17 @@
+/**
+ * 覆盖配置
+ */
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, ".", dir);
+}
+
 module.exports = function override(config, env) {
   // do stuff with the webpack config...
+  config.resolve.alias = {
+    // 配置别名-使用@来表示'src'的绝对路径了
+    "@": resolve("src")
+  };
   return config;
 };
 const { override, fixBabelImports, addLessLoader } = require("customize-cra");
